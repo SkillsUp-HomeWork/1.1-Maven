5e45937f2b41a4c33d02d13b565a3efe4fc716aa
admin <xaker.dnepr@gmail.com>
2015-11-24 22:17:45 +0200

    first commit

---
+++ src/main/java/api/MusicSchoolOrganize.java
@@ -0,0 +1,8 @@
+package api;
+
+/**
+ * Created by admin on 24.11.2015.
+ */
+public interface MusicSchoolOrganize {
+    void someMethod();
+}
---
+++ src/main/java/app/MusicSchoolOrganizeImpl.java
@@ -0,0 +1,12 @@
+package app;
+
+import api.MusicSchoolOrganize;
+
+/**
+ * Created by admin on 24.11.2015.
+ */
+public class MusicSchoolOrganizeImpl implements MusicSchoolOrganize {
+    public void someMethod() {
+
+    }
+}
